c8eb1c0c257011dd10f3dc8322cf985255093c34
Sean Shirzad <mehrdad.rezaieshirzad@georgebrown.ca>
2017-10-20 16:34:58 -0400

    Final Version of Classes and Scripts
    All files with the "lastVersion" suffix include the finalized scripts which have complete game's functionalities.

---
+++ oceanController_lastVersion.cs
@@ -0,0 +1,50 @@
+﻿/*
++-----------------------------------------------------------+
+/ Source file name: oceanController.ca						/
+/ Author’s name: Mehrdad Rezaieshirzad						/
+/ Last Modified by: Mehrdad Rezaieshirzad					/                     
+/ Date last Modified: October 16, 2017						/                       
+/ Program description: This class responsibble for making	/  
+/movement simulation										/
+/Revision History: N/A										/
++-----------------------------------------------------------+
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oceanController : MonoBehaviour {
+	[SerializeField]
+	private float speed = 5f;
+	[SerializeField]
+	private float startX;
+	[SerializeField]
+	private float endX;
+
+	private Transform _transform;
+	private Vector2 _currentPosition;
+	// Use this for initialization
+	void Start () {
+		_transform = gameObject.GetComponent<Transform> ();
+		_currentPosition = _transform.position;
+		Reset ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_currentPosition = _transform.position;
+		_currentPosition -= new Vector2 (speed, 0);
+
+		if (_currentPosition.x < endX) {
+			Reset ();
+		}
+		_transform.position = _currentPosition;
+	}
+
+	private void Reset(){
+		_currentPosition = new Vector2 (startX, 0);
+	
+	}
+}
