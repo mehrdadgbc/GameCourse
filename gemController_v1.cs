45ba6797ede9104ae6ece30476b14d0f8711f4a9
Sean Shirzad <mehrdad.rezaieshirzad@georgebrown.ca>
2017-10-20 16:28:30 -0400

    Add files via upload

---
+++ gemController_v1.cs
@@ -0,0 +1,45 @@
+﻿/*
++-----------------------------------------------------------+
+/ Source file name: gemController.ca						/
+/ Author’s name: Mehrdad Rezaieshirzad						/
+/ Last Modified by: Mehrdad Rezaieshirzad					/                     
+/ Date last Modified: October 16, 2017						/                       
+/ Program description: This class responsibble for making	/  
+/gems randomly in all over the game's scence				/
+/Revision History: N/A										/
++-----------------------------------------------------------+
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gemController : MonoBehaviour {
+	[SerializeField]
+	private float speed = 5f;
+	[SerializeField]
+	private float startX;
+	[SerializeField]
+	private float endX;
+
+	private Transform _transform;
+	private Vector2 _currentPosition;
+	// Use this for initialization
+	void Start () {
+		_transform = gameObject.GetComponent<Transform> ();
+		_currentPosition = _transform.position;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_currentPosition = _transform.position;
+		_currentPosition -= new Vector2 (speed,0);
+
+		if (_currentPosition.x < endX) {
+			Reset ();
+		}
+		_transform.position = _currentPosition;
+	}
+
+}
